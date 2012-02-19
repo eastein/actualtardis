3555fb08590fea297e5262ee5732d7406b5cfbf5
Eric Stein <toba@des.truct.org>
2012-02-19 05:28:40 -0600

    more mapped controls, spdt parsing

--- tardis.py
+++ tardis.py
@@ -5,19 +5,61 @@ import pprint
 from ramirez.iod import iodclient
 from ramirez.iod import iod_proto
 
-class Tardis(object) :
-	mappings = {
-		0 : "master stop",
-		1 : "arm",
-		2 : "hv off",
-		5 : "master start",
-		6 : "interlock open",
-		7 : "hv on",
-		10 : "current",
-		11 : "dial",
-	}
+class SPDT(object) :
+	"""
+	mapping shall be a dictionary from channel number to string state name. if channel number is None,
+	this implies that this state shall be true if none of the channels are active.
+	"""
+	def __init__(self, name, mapping) :
+		self.name = name
+		self.mapping = mapping
+		self._state = dict()
 
+	def measurement(self, channel, value) :
+		if not self._state :
+			for k in self.mapping :
+				if k is not None :
+					self._state[k] = False
+		self._state[channel] = value
+
+	@property
+	def state(self) :
+		if not self._state :
+			return None
+
+		for c in self.mapping :
+			if c is not None :
+				if self._state[c] :
+					return self.mapping[c]
+
+		if None in self.mapping :
+			return self.mapping[None]
+
+		return None
+
+class Tardis(object) :
 	def __init__(self) :
+		mode = SPDT("mode", {
+			8 : "ext",
+			9 : "cont",
+			17 : "reset",
+			None : "man",
+		})
+
+		self.mappings = {
+			0 : "master stop",
+			1 : "arm",
+			2 : "hv off",
+			5 : "master start",
+			6 : "interlock open",
+			7 : "hv on",
+			8 : mode,
+			9 : mode,
+			10 : "current",
+			11 : "dial",
+			17 : mode,
+		}
+
 		self.channels = [
 			(0, iod_proto.CHANNELTYPE_DIGITAL, lambda v: not v),
 			(1, iod_proto.CHANNELTYPE_DIGITAL, None),
@@ -25,8 +67,11 @@ class Tardis(object) :
 			(5, iod_proto.CHANNELTYPE_DIGITAL, None),
 			(6, iod_proto.CHANNELTYPE_DIGITAL, lambda v: not v),
 			(7, iod_proto.CHANNELTYPE_DIGITAL, None),
+			(8, iod_proto.CHANNELTYPE_DIGITAL, None),
+			(9, iod_proto.CHANNELTYPE_DIGITAL, None),
 			(10, iod_proto.CHANNELTYPE_ANALOG, lambda v: self.translate_range(v, 3.3642578125, 0.0, 0.0, 5.0)),
 			(11, iod_proto.CHANNELTYPE_ANALOG, lambda v: self.translate_range(v, 0.0, 3.26171875, 0.0, 1000)),
+			(17, iod_proto.CHANNELTYPE_DIGITAL, None),
 		]
 		self.chans = [(n,t) for (n,t,f) in self.channels]
 		self.chantransforms = dict([(n,f) for (n,t,f) in self.channels])
@@ -53,10 +98,19 @@ class Tardis(object) :
 			else :
 				d[n] = ct(v)
 
+		#pprint.pprint(d)
+
 		print 'sample:'
 		for c in d :
-			print ' %s: %s' % (self.mappings[c].ljust(20), d[c])
-			
+			mapped = self.mappings[c]
+			if isinstance(mapped, SPDT) :
+				mapped.measurement(c, d[c])
+			else :
+				print ' %s: %s' % (mapped.ljust(20), d[c])
+
+		for m in set([self.mappings[m] for m in self.mappings if isinstance(self.mappings[m], SPDT)]) :
+			print ' %s: %s' % (m.name.ljust(20), m.state)
+
 if __name__ == '__main__' :
 	t = Tardis()
 	while True :
