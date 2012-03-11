ef006637c7798945721cb0474610b1009661d49d
Eric Stein <toba@des.truct.org>
2012-03-11 00:42:44 -0600

    working on an event system for the control panel

--- tardis.py
+++ tardis.py
@@ -7,13 +7,58 @@ from ramirez.iod import iod_proto
 
 # TODO the manual button is mixed up with the mode:cont switch, need to reroute something.
 
-class SPDT(object) :
+class Listener(object) :
+	def __init__(self) :
+		pass
+
+	def event(self, event) :
+		print event
+
+class InputEvent(object) :
+	def __init__(self, input_object, old_value, new_value) :
+		self.input_object = input_object
+		self.old_value = old_value
+		self.new_value = new_value
+
+	def __repr__(self) :
+		return '<InputEvent %s %s -> %s>' % (self.input_object, self.old_value, self.new_value)
+
+class Input(object) :
+	def __init__(self, name) :
+		self.listeners = list()
+		self.name = name
+		self._oldstate = None
+
+	def register(self, listener) :
+		self.listeners.append(listener)
+
+	def __repr__(self) :
+		return '<%s %s>' % (self.__class__.__name__, self.name)
+
+	"""
+	Check for change, save for the next change check.
+	If changes, create relevant events.
+	"""
+	def check(self) :
+		# evaluate
+		state = self.state
+
+		# compare
+		if state != self._oldstate :
+			event = InputEvent(self, self._oldstate, state)
+			for listener in self.listeners :
+				listener.event(event)
+
+		# save
+		self._oldstate = state
+
+class SPDT(Input) :
 	"""
 	mapping shall be a dictionary from channel number to string state name. if channel number is None,
 	this implies that this state shall be true if none of the channels are active.
 	"""
 	def __init__(self, name, mapping) :
-		self.name = name
+		Input.__init__(self, name)
 		self.mapping = mapping
 		self._state = dict()
 
@@ -39,6 +84,18 @@ class SPDT(object) :
 
 		return None
 
+class Singular(Input) :
+	def __init__(self, name) :
+		Input.__init__(self, name)
+		self._state = None
+	
+	def measurement(self, value) :
+		self._state = value
+
+	@property
+	def state(self) :
+		return self._state
+
 class Tardis(object) :
 	def __init__(self) :
 		mode = SPDT("mode", {
@@ -49,20 +106,23 @@ class Tardis(object) :
 		})
 
 		self.mappings = {
-			0 : "master stop",
-			1 : "arm",
-			2 : "hv off",
-			4 : "shutter",
-			5 : "master start",
-			6 : "interlock open",
-			7 : "hv on",
+			0 : Singular("master stop"),
+			1 : Singular("arm"),
+			2 : Singular("hv off"),
+			4 : Singular("shutter"),
+			5 : Singular("master start"),
+			6 : Singular("interlock open"),
+			7 : Singular("hv on"),
 			8 : mode,
 			9 : mode,
-			10 : "current",
-			11 : "dial",
+			10 : Singular("current"),
+			11 : Singular("dial"),
 			17 : mode,
-			18 : "magic",
+			18 : Singular("magic"),
 		}
+		listener = Listener()
+		for i in self.mappings.values() :
+			i.register(listener)
 
 		self.channels = [
 			(0, iod_proto.CHANNELTYPE_DIGITAL, lambda v: not v),
@@ -105,20 +165,25 @@ class Tardis(object) :
 			else :
 				d[n] = ct(v)
 
-		print 'sample:'
+		#print 'sample:'
 		for c in d :
 			mapped = self.mappings[c]
 			if isinstance(mapped, SPDT) :
 				mapped.measurement(c, d[c])
 			else :
-				dat[mapped] = d[c]
-				print ' %s: %s' % (mapped.ljust(20), d[c])
+				mapped.measurement(d[c])
+				#print ' %s: %s' % (mapped.name.ljust(20), d[c])
 
 		for m in set([self.mappings[m] for m in self.mappings if isinstance(self.mappings[m], SPDT)]) :
-			dat[m.name] = m.state
-			print ' %s: %s' % (m.name.ljust(20), m.state)
+			#print ' %s: %s' % (m.name.ljust(20), m.state)
+			pass
 
-		pprint.pprint(dat)
+		for m in set(self.mappings.values()) :
+			m.check()
+
+		dat = zip([(self.mappings[m].name, self.mappings[m]) for m in self.mappings])
+
+		#pprint.pprint(dat)
 
 if __name__ == '__main__' :
 	t = Tardis()
